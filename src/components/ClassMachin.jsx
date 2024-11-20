5f7cdd80a74360b1b71557a1c9fda7a28dd2b33f
GadTzarfati <gadtzarfati26@gmail.com>
2024-11-20 16:47:13 +0200

    xcv

--- src/components/ClassMachin.jsx
+++ src/components/ClassMachin.jsx
@@ -1,31 +1,22 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { FaPowerOff } from 'react-icons/fa';
 
-const Machine = () => {
-    const [isTurnOn, setIsTurnOn] = useState(false);
 
-    const togglePower = () => {
-        setIsTurnOn((prev) => !prev);
+class Machin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isTurnOn: false,};
+    }
+    togglePower = () => {
+        this.setState((prevState) => ({isTurnOn: !prevState.isTurnOn, }));
     };
-
-    return (
-        <button
-        onClick={togglePower}
-        className={`absolute top-4 right-4 p-2 rounded-full shadow-lg transition-transform transform hover:-translate-y-1 flex items-center justify-center w-10 h-10 ${
-            isTurnOn ? 'bg-red-500 hover:bg-red-600' : 'bg-gray-500 hover:bg-gray-600'
-        }`}
-      >
-        <FaPowerOff size={18} className="text-white" />
-      </button>
-      
-    );
-};
-
-const ClassMachin = () => {
-    return (
-        <>
-        </>
-    )
+    renderPowerButton() {
+        const { isTurnOn } = this.state;
+        return (
+            <button onClick={this.togglePower}
+                className={`absolute top-4 right-4 p-2 rounded-full shadow-lg transition-transform transform hover:-translate-y-1 flex items-center justify-center w-10 h-10 ${isTurnOn ? 'bg-red-500 hover:bg-red-600' : 'bg-gray-500 hover:bg-gray-600'}`} >
+                <FaPowerOff size={18} className="text-white" /> </button>
+        );
+    }
 }
-
-export default ClassMachin
+export default Machin;
