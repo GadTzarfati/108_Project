2cfac521653ae78e876daa83e8f976fcbe037ce4
GadTzarfati <gadtzarfati26@gmail.com>
2024-11-20 16:29:47 +0200

    rtyu

--- src/components/ClassMachin.jsx
+++ src/components/ClassMachin.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaPowerOff } from 'react-icons/fa';
+
 const Machine = () => {
     const [isTurnOn, setIsTurnOn] = useState(false);
 
@@ -8,14 +10,14 @@ const Machine = () => {
 
     return (
         <button
-            onClick={togglePower}
-            className={`absolute top-4 right-4 p-2 rounded-full shadow-lg transition-transform transform hover:-translate-y-1 flex items-center justify-center w-10 h-10 ${
-                isTurnOn ? 'bg-red-500 hover:bg-red-600' : 'bg-gray-500 hover:bg-gray-600'
-            }`}
-        >
-            {isTurnOn ? 'On' : 'Off'}
-        </button>
-        
+        onClick={togglePower}
+        className={`absolute top-4 right-4 p-2 rounded-full shadow-lg transition-transform transform hover:-translate-y-1 flex items-center justify-center w-10 h-10 ${
+            isTurnOn ? 'bg-red-500 hover:bg-red-600' : 'bg-gray-500 hover:bg-gray-600'
+        }`}
+      >
+        <FaPowerOff size={18} className="text-white" />
+      </button>
+      
     );
 };
 
